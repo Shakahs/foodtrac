6e867fabdfa53d2f19ba1fe52e0c56b29a145dad
Shakeel Saleemi <shakahs@gmail.com>
2017-05-20 13:00:09 -0700

    First implementation of Swaggerized API  controllers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerize = require('swaggerize-express');
+const swaggerSpec = require('../dev/api.json');
+const morgan = require('morgan');
 
 const app = express();
 app.use('/static', express.static('static'));
 app.use(bodyParser.json());
+
+delete swaggerSpec.host;
+app.use(swaggerize({
+  api: swaggerSpec,
+  handlers: './controllers',
+}));
+
+if (app.get('env') === 'production') {
+  app.use(morgan('common', { skip(req, res) { return res.statusCode < 400; }, stream: `${__dirname}/../morgan.log` }));
+} else {
+  app.use(morgan('dev'));
+}
 
 app.get('/hello', (req, res) => {
   res.end('hello world');
