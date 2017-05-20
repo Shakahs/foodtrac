36a54a1072191a3490c49df1c6c818f706c150e3
Keebin <kirikliu@gmail.com>
2017-05-19 17:03:53 -0700

    Add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "rules": {
+        "no-underscore-dangle": [2, { "allowAfterThis": true, allow: ["_*"] }],
+        "class-methods-use-this": 0,
+        "no-static-element-interactions": 0,
+    }
+};
