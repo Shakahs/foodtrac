6eb16246656f49c7a61e943ce3d757efc7a1b67c
Keebin <kirikliu@gmail.com>
2017-05-22 15:37:30 -0700

    Fix outdated relationMapping reference in /foodtrucks/{truckId}/location controller

--- server/controllers/foodtrucks/{truckId}/location.js
+++ server/controllers/foodtrucks/{truckId}/location.js
@@ -2,11 +2,12 @@ const Trucks = require('../../../db/trucks.model');
 
 module.exports = {
   post(req, res) {
-    req.body.start = req.body.start.toISOString();
+    // req.body.start = req.body.start.toISOString();
     Trucks.query()
       .findById(req.params.truckId)
       // TODO: Filter by valid start/end time
-      .then(truck => truck.$relatedQuery('location_timeline').insert(req.body))
+      // TODO: Find way to add custom start time without starting a new query
+      .then(truck => truck.$relatedQuery('locations').relate(req.body.location_id))
       .then(() => res.send(`Inserted new location timeline entry for truck #${req.params.truckId}`))
       .catch(e => res.status(400).send(e.message));
   },
