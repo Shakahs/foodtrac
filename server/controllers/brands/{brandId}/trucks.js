8b212647c866748ec877b5ae1f25e2b19e5fb29f
Keebin <kirikliu@gmail.com>
2017-05-22 13:03:09 -0700

    Add /brands/{brandId}/trucks POST controller

--- server/controllers/brands/{brandId}/trucks.js
+++ server/controllers/brands/{brandId}/trucks.js
@@ -2,6 +2,7 @@ const Brands = require('../../../db/brands.model');
 
 module.exports = {
   get(req, res) {
+    // eager load with current location timeline if exists
     Brands.query()
       .findById(req.params.brandId)
       .then(brand => brand.$relatedQuery('trucks'))
