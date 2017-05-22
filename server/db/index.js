338bb39e52b5f24b4d203b5d290d7dddfbe2756d
Shakeel Saleemi <shakahs@gmail.com>
2017-05-21 17:17:34 -0700

    Add db schema index file for easy access to models

---
+++ server/db/index.js
@@ -0,0 +1,7 @@
+import { Users } from './users.model';
+import { Brands } from './brands.model';
+import { Trucks } from './trucks.model';
+import { Locations } from './locations.model';
+import { LocationTimeline } from './locationtimeline.model';
+
+export { Users, Brands, Trucks, Locations, LocationTimeline };
