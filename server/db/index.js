5569824687838d17d1c2fec2c16830555bd2592e
Shakeel Saleemi <shakahs@gmail.com>
2017-05-21 18:09:29 -0700

    Remove db index.js file which apparently doesn't work with Node

--- server/db/index.js
+++
@@ -1,7 +0,0 @@
-import { Users } from './users.model';
-import { Brands } from './brands.model';
-import { Trucks } from './trucks.model';
-import { Locations } from './locations.model';
-import { LocationTimeline } from './locationtimeline.model';
-
-export { Users, Brands, Trucks, Locations, LocationTimeline };
