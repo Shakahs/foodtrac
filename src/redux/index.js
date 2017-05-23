c078e81eb7837481f8cd7d413aa1796233633bf0
Shakeel Saleemi <shakahs@gmail.com>
2017-05-23 16:18:57 -0700

    Label Redux store user reducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,8 +3,8 @@ import { reducer as mapReducer } from './MapSearch';
 import { reducer as userReducer } from './user';
 
 const combinedReducer = combineReducers({
-  userReducer,
   mapReducer,
+  user: userReducer,
 });
 
 export default combinedReducer;
