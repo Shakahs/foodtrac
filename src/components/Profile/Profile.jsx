e717420654d0973b23d737e3ce39d596af0fa35f
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-05-24 08:41:32 -0700

    Change brand get request in Profile to use eager loading

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -20,28 +20,16 @@ class Profile extends Component {
 
   componentDidMount() {
     this.getBrandDetail();
-    this.getFoodGenre();
-    this.getBrandTrucks();
   }
 
   getBrandDetail() {
-    axios.get(`/api/brands/${this.state.brandId}`)
+    axios.get(`/api/brands/${this.state.brandId}?eager=true`)
       .then((res) => {
-        this.setState({ brandName: res.data[0].name });
-        this.setState({ brandDescription: res.data[0].description });
+        this.setState({ brandName: res.data.name });
+        this.setState({ brandDescription: res.data.description });
+        this.setState({ foodGenre: res.data.food_genres.name });
+        this.setState({ trucks: res.data.trucks });
       })
-      .catch(err => console.log(err));
-  }
-
-  getFoodGenre() {
-    axios.get(`/api/foodgenre/${this.state.brandId}`)
-      .then(res => this.setState({ foodGenre: res.data[0].name }))
-      .catch(err => console.log(err));
-  }
-
-  getBrandTrucks() {
-    axios.get(`/api/brands/${this.state.brandId}/trucks`)
-      .then(res => this.setState({ trucks: res.data }))
       .catch(err => console.log(err));
   }
 
@@ -70,7 +58,7 @@ class Profile extends Component {
 Profile.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      brandId: PropTypes.number,
+      brandId: PropTypes.string,
     }),
   }).isRequired,
 };
