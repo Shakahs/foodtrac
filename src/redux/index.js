8ccc792ad5010772b0eaebfa4032f8f71178132f
Shakeel Saleemi <shakahs@gmail.com>
2017-05-23 17:12:35 -0700

    Install redux-form

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import { reducer as mapReducer } from './MapSearch';
 import { reducer as userReducer } from './user';
 
 const combinedReducer = combineReducers({
   mapReducer,
   user: userReducer,
+  form: formReducer,
 });
 
 export default combinedReducer;
