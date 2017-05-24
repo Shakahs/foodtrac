603d07df61b25c7414d33a06fbc96012830502a8
Keebin <kirikliu@gmail.com>
2017-05-24 11:11:07 -0700

    Refactor MapSearch reducer and saga files to match controller change

--- src/redux/MapSearch/reducer.js
+++ src/redux/MapSearch/reducer.js
@@ -5,6 +5,7 @@ export const MAP_FAILURE = 'MAP_FAILURE';
 
 const initialState = {
   markers: [],
+  trucks: [],
   error: null,
   fetching: false,
 };
@@ -14,7 +15,11 @@ export default function reducer(state = initialState, action) {
     case MAP_REQUEST:
       return Object.assign({}, state, { fetching: true });
     case MAP_SUCCESS:
-      return Object.assign({}, state, { fetching: false, markers: action.markers });
+      return Object.assign({}, state, {
+        fetching: false,
+        markers: action.markers,
+        trucks: action.trucks,
+      });
     case MAP_FAILURE:
       return Object.assign({}, state, { fetching: false, error: action.error });
     default:
@@ -29,9 +34,10 @@ export const mapRequest = (lat, lng, dist) => ({
   dist,
 });
 
-export const mapSuccess = markers => ({
+export const mapSuccess = (markers, trucks) => ({
   type: MAP_SUCCESS,
   markers,
+  trucks,
 });
 
 export const mapFailure = error => ({
