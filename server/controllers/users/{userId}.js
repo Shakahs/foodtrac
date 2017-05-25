bc0c2d1c14ddcddcfb327517c2296ab49b336ea9
Keebin <kirikliu@gmail.com>
2017-05-24 17:53:16 -0700

    Use findById for single result and eager load brands in /user/{userId} controllers

--- server/controllers/users/{userId}.js
+++ server/controllers/users/{userId}.js
@@ -3,24 +3,24 @@ const Users = require('../../db/users.model');
 module.exports = {
   get(req, res) {
     Users.query()
-      .where('id', '=', req.params.userId)
+      .findById(req.params.userId)
+      .eager('brands')
       .then((user) => {
-        const thisUser = user[0];
-        thisUser.is_truck_owner = Boolean(thisUser.is_truck_owner);
-        res.status(200).json(thisUser);
+        user.is_truck_owner = Boolean(user.is_truck_owner); // eslint-disable-line no-param-reassign
+        res.status(200).json(user);
       })
       .catch(e => console.log('Error inserting new user:', e));
   },
   put(req, res) {
     Users.query()
-      .where('id', '=', req.params.userId)
+      .findById(req.params.userId)
       .patch(req.body)
       .then(user => res.status(200).json(user))
       .catch(e => console.log('Error updating user:', e));
   },
   delete(req, res) {
     Users.query()
-      .where('id', '=', req.params.userId)
+      .findById(req.params.userId)
       .delete()
       .then(() => res.status(200))
       .catch(e => console.log('Error deleting user:', e));
