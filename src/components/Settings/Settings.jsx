1ec49e90e5ca5398883ed2cddc0d10ad21d69f90
Keebin <kirikliu@gmail.com>
2017-05-24 18:01:19 -0700

    Render settings sidebar and nested router switch in Settings component

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import SettingsSideBar from './SettingsSideBar';
+import AddBrandsView from '../common/AddBrandsView';
 
 const Settings = () => (
-  <div>Settings</div>
+  <div>
+    <p>Settings</p>
+    <SettingsSideBar />
+    <Switch>
+      <Route path="/settings/addbrand" component={AddBrandsView} />
+    </Switch>
+  </div>
 );
 
 export default Settings;
