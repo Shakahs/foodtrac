5883e0eb27f4d5bef133365d5ac96171069408f4
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-05-24 20:38:34 -0700

    add link to profile

--- src/components/common/TruckEntry.jsx
+++ src/components/common/TruckEntry.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Col } from 'react-flexbox-grid';
 import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const TruckEntry = props => (
   <Col xs={12} sm={12} md={6} lg={6}>
-    {console.log(props.truck)}
+    {console.log('in truck entry', props.truck.brands)}
     <Paper className="truckEntry">
       <p>{props.truck.brands.name}: {props.truck.name !== 'null' ? <em>{props.truck.name}</em> : null}</p>
       <p>Type of food: {props.truck.brands.food_genres.name}</p>
       <p>Current locations: {props.truck.locations[0].address}</p>
+      <Link to={`/brand/${props.truck.brand_id}/trucks`}>
+        <RaisedButton label="Go to Profile" />
+      </Link>
     </Paper>
   </Col>
 );
