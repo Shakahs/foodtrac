48b08f0f8a1ca3e4163d58fca54674b6cd255422
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-05-25 12:31:15 -0700

    Add basic manage form on profile

--- src/components/Profile/ManageBrand.jsx
+++ src/components/Profile/ManageBrand.jsx
@@ -1,30 +1,61 @@
-import React from 'react';
-import { Field, reduxForm } from 'redux-form';
-import RaisedButton from 'material-ui/RaisedButton';
+import React, { Component } from 'react';
+import { TextField, SelectField, MenuItem, RaisedButton } from 'material-ui';
 
-const ManageBrand = () => (
-  <div>
-    <form>
-      <div>Basic Information:</div>
-      <div>
-        <label htmlFor="brandName">Brand Name:</label>
-        <Field name="brandName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="brandDescription">Brand Description:</label>
-        <Field name="brandDescription" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="foodGenre">Type of food:</label>
-        <Field name="foodGenre" component="input" type="text" />
-      </div>
-      <RaisedButton label="Save Changes" />
-    </form>
-  </div>
-);
+class ManageBrand extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      description: '',
+      foodGenre: '',
+    };
+  }
 
-const ManageBrandForm = reduxForm({
-  form: 'manageBrand',
-})(ManageBrand);
+  handleSave() {
+    console.log('in handle save', this.state);
+  }
 
-export default ManageBrandForm;
+  render() {
+    return (
+      <div>
+        <form>
+          <div>Change Basic Information:</div>
+          <br />
+          <br />
+          <TextField
+            hintText="Change Brand Name"
+            onChange={(e, val) => this.setState({ name: val })}
+            value={this.state.name}
+          />
+          <br />
+          <TextField
+            hintText="Change Brand Description"
+            onChange={(e, val) => this.setState({ description: val })}
+            value={this.state.description}
+          />
+          <br />
+          <SelectField
+            floatingLabelText="Change Food Genre"
+            value={this.state.foodGenre}
+            onChange={(e, val) => this.setState({ foodGenre: val })}
+            autoWidth
+          >
+            <MenuItem value={1} primaryText="Burgers" />
+            <MenuItem value={2} primaryText="Pho" />
+            <MenuItem value={3} primaryText="Mexican" />
+            <MenuItem value={4} primaryText="BBQ" />
+            <MenuItem value={5} primaryText="Thai" />
+          </SelectField>
+          <br />
+          <br />
+          <RaisedButton
+            label="Save Changes"
+            onClick={() => this.handleSave()}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ManageBrand;
