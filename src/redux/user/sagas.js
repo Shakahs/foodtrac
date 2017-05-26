215a17ed8d827b20e01db121081524bd6a62b0b6
Shakeel Saleemi <shakahs@gmail.com>
2017-05-26 13:08:23 -0700

    Fix auth0 metadata submission to server on signup/login

--- src/redux/user/sagas.js
+++ src/redux/user/sagas.js
@@ -7,11 +7,11 @@ export default function* watchLoginSuccess() {
   while (true) {
     const { profileData } = yield take(authActions.LOGIN_SUCCESS);
     const postData = { auth0_id: profileData.user_id };
-    console.log(profileData);
-    postData.is_truck_owner = !!((profileData.user_metadata &&
+    postData.is_truck_owner = (profileData.user_metadata &&
       profileData.user_metadata.signed_up_as_truck_owner &&
-      Boolean(Number(profileData.user_metadata.signed_up_as_truck_owner))));
+      Boolean(Number(profileData.user_metadata.signed_up_as_truck_owner)));
     const userData = yield call(axios.post, '/api/users/', postData);
+    userData.data.is_truck_owner = Boolean(userData.data.is_truck_owner);
     yield put(actions.userReceived(userData.data));
   }
 }
