8f7cb49c430d52029b088e84d2e639df01aac5eb
Keebin <kirikliu@gmail.com>
2017-05-25 21:13:40 -0700

    Add follow button to truck entry and selectively render go to profile button based on router path

--- src/components/common/TruckEntry.jsx
+++ src/components/common/TruckEntry.jsx
@@ -12,13 +12,12 @@ const TruckEntry = props => (
       <p>{props.truck.brands.name}: {props.truck.name !== 'null' ? <em>{props.truck.name}</em> : null}</p>
       <p>Type of food: {props.truck.brands.food_genres.name}</p>
       <p>Current locations: {props.truck.locations[0].address}</p>
-      {props.truck.brands.fromProfile ?
-        null :
-        <Link to={`/brand/${props.truck.brand_id}/trucks`}>
-          <RaisedButton label="Go to Profile" />
-        </Link>
-      }
-      <FollowButton brandId={props.truck.brand_id} user={props.user} />
+      {props.path === '/brand/:brandId/trucks'
+      ? null
+      : <Link to={`/brand/${props.truck.brand_id}/trucks`}>
+        <RaisedButton label="Go to Profile" />
+      </Link>}
+      <FollowButton brandId={props.truck.brand_id} user={props.user} path={props.path} />
     </Paper>
   </Col>
     );
@@ -26,6 +25,7 @@ const TruckEntry = props => (
 TruckEntry.propTypes = {
   truck: propSchema.truck,
   user: propSchema.user,
+  path: propSchema.path,
 };
 
 export default TruckEntry;
