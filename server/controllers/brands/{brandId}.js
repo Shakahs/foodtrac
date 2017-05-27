d02434de7502e3d156064e5f7195eb9fe78b6b61
Keebin <kirikliu@gmail.com>
2017-05-26 19:57:52 -0700

    Add controller logic to only return truck locations that are still valid

--- server/controllers/brands/{brandId}.js
+++ server/controllers/brands/{brandId}.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const Brands = require('../../db/brands.model');
 
 module.exports = {
@@ -9,12 +10,31 @@ module.exports = {
       .catch(e => console.log('Error updating brand:', e));
   },
   get(req, res) {
-    // brand, food genre, trucks with location
+    // TODO: modify trucks.locations eager to only get current location
     const eagerOption = req.query.eager ? '[trucks.locations, food_genres]' : '';
+    const currentTime = new Date();
+    const latestValidTime = new Date(currentTime - 28800000);
     Brands.query()
-      .eager(eagerOption)
       .findById(req.params.brandId)
-      .then(brand => res.status(200).json(brand))
+      .eagerAlgorithm(Brands.WhereInEagerAlgorithm)
+      .eager(eagerOption)
+      .modifyEager('trucks.locations', (builder) => {
+        builder
+          .andWhereBetween('start', [latestValidTime.toISOString(), currentTime.toISOString()])
+          .andWhere('end', 0)
+          .orWhere('end', '>', currentTime.toISOString())
+          .orderBy('start', 'desc');
+      })
+      .then((brand) => {
+        brand.trucks = _.filter(brand.trucks, (truck) => { // eslint-disable-line no-param-reassign
+          if (truck.locations.length > 0) {
+            truck.locations = truck.locations[0]; // eslint-disable-line no-param-reassign
+            return true;
+          }
+          return false;
+        });
+        res.status(200).json(brand);
+      })
       .catch(e => console.log('Error fetching brand:', e));
   },
 };
