feb61d1fa076f4968c6ba02649508908259d6859
Shakeel Saleemi <shakahs@gmail.com>
2017-05-27 12:43:12 -0700

    Finish setting up new truck owner redirect on first login only

--- src/redux/user/sagas.js
+++ src/redux/user/sagas.js
@@ -3,17 +3,18 @@ import axios from 'axios';
 import { actions } from './index';
 import { actions as authActions } from '../auth';
 
+const moment = require('moment');
+
 export default function* watchLoginSuccess() {
   while (true) {
     const { profileData } = yield take(authActions.LOGIN_SUCCESS);
     const postData = { auth0_id: profileData.user_id };
-    postData.is_truck_owner = (profileData.user_metadata &&
-      profileData.user_metadata.signed_up_as_truck_owner &&
-      Boolean(Number(profileData.user_metadata.signed_up_as_truck_owner)));
+    postData.is_truck_owner = (Boolean(Number(profileData.user_metadata.signed_up_as_truck_owner)));
     const userData = yield call(axios.post, '/api/users/', postData);
     userData.data.is_truck_owner = Boolean(userData.data.is_truck_owner);
     yield put(actions.userReceived(userData.data));
-    if (userData.data.is_truck_owner) {
+    if (userData.data.is_truck_owner &&
+      moment(profileData.created_at).isAfter(moment().subtract(1, 'minute'))) {
       yield put(actions.redirectAddBrandEnable());
     }
   }
