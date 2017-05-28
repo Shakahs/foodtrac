c1b69545ce6f5064a3386bf3af2937c605fa0093
Shakeel Saleemi <shakahs@gmail.com>
2017-05-27 18:14:27 -0700

    Refactor Manage profile basic form with redux-form

--- src/components/Profile/ManageBasic.jsx
+++ src/components/Profile/ManageBasic.jsx
@@ -1,114 +1,61 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React from 'react';
+
 import { Link } from 'react-router-dom';
-import { TextField, SelectField, MenuItem, RaisedButton } from 'material-ui';
-import axios from 'axios';
-import { actions as userActions } from '../../redux/user';
+import { MenuItem, RaisedButton } from 'material-ui';
+
+import { SelectField, TextField } from 'redux-form-material-ui';
+import { Field, reduxForm } from 'redux-form';
+
 import propSchema from '../common/PropTypes';
 
-class ManageBasic extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: '',
-      description: '',
-      food_genre_id: 0,
-    };
-  }
 
-  handleInfoEdit() {
-    const update = {};
-    if (this.state.name !== '') {
-      update.name = this.state.name;
-    }
-    if (this.state.description !== '') {
-      update.description = this.state.description;
-    }
-    if (this.state.food_genre_id > 0) {
-      update.food_genre_id = this.state.food_genre_id;
-    }
-    if (Object.keys(update).length > 0) {
-      axios.put(`/api/brands/${this.props.brandId}`, update)
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
-    }
-  }
-
-  handleReduxUpdate() {
-    const newBrandInfo = Object.assign({}, this.props.user.brands[0]);
-    if (this.state.name !== '') {
-      newBrandInfo.name = this.state.name;
-    }
-    if (this.state.description !== '') {
-      newBrandInfo.description = this.state.description;
-    }
-    if (this.state.food_genre_id > 0) {
-      newBrandInfo.food_genre_id = this.state.food_genre_id;
-    }
-    this.props.userActions.brandInfoUpdate(newBrandInfo);
-  }
-
-  handleSave() {
-    this.handleInfoEdit();
-    this.handleReduxUpdate();
-    this.props.getBrand(this.props.brandId);
-  }
-
-  render() {
-    return (
-      <div>
-        <TextField
-          hintText="Change Brand Name"
-          onChange={(e, val) => this.setState({ name: val })}
-          value={this.state.name}
-        />
-        <br />
-        <TextField
-          hintText="Change Brand Description"
-          onChange={(e, val) => this.setState({ description: val })}
-          value={this.state.description}
-          multiLine
-        />
-        <br />
-        <SelectField
-          floatingLabelText="Change Food Genre"
-          onChange={(e, i, val) => this.setState({ food_genre_id: val })}
-          value={this.state.food_genre_id}
-        >
-          <MenuItem value={0} />
-          {this.props.foodGenres.map(genre =>
-            <MenuItem key={genre.id} value={genre.id} primaryText={genre.name} />,
-                )}
-        </SelectField>
-        <br />
-        <br />
-        <Link to={`/brand/${this.props.brandId}/trucks`}>
-          <RaisedButton
-            label="Save Changes"
-            onClick={() => this.handleSave()}
-          />
-        </Link>
-      </div>
-    );
-  }
-}
+const ManageBasic = props => (
+  <div>
+    <Field
+      name="name"
+      component={TextField}
+      type="text"
+      hintText="Change Brand Name"
+      floatingLabelText="Change Brand Name"
+    />
+    <br />
+    <Field
+      name="description"
+      component={TextField}
+      type="text"
+      hintText="Change Brand Description"
+      floatingLabelText="Change Brand Description"
+    />
+    <br />
+    <Field
+      name="food_genre_id"
+      component={SelectField}
+      hintText="Change Food Genre"
+      floatingLabelText="Change Food Genre"
+    >
+      <MenuItem value="{0}" />
+      {props.foodGenres.map(genre =>
+        <MenuItem key={genre.id} value={genre.id} primaryText={genre.name} />,
+      )}
+    </Field>
+    <br />
+    <br />
+    <Link to={`/brand/${props.brandId}/trucks`}>
+      <RaisedButton
+        label="Save Changes"
+        onClick={props.handleSubmit}
+      />
+    </Link>
+  </div>
+);
 
 ManageBasic.propTypes = {
   brandId: propSchema.brandId,
   foodGenres: propSchema.foodGenres,
-  user: propSchema.user,
-  userActions: propSchema.userActions,
-  getBrand: propSchema.getBrand,
+  handleSubmit: propSchema.handleSubmit,
 };
 
-const mapStateToProps = ({ foodGenresReducer, user }) => {
-  const { foodGenres } = foodGenresReducer;
-  return { foodGenres, user };
-};
+export default reduxForm({
+  form: 'SignUp', // a unique name for this form
+})(ManageBasic);
 
-const mapDispatchToProps = dispatch => ({
-  userActions: bindActionCreators(userActions, dispatch),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ManageBasic);
--- src/components/Profile/ManageBrand.jsx
+++ src/components/Profile/ManageBrand.jsx
@@ -1,31 +1,109 @@
 import React from 'react';
 import { Tabs, Tab } from 'material-ui';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import axios from 'axios';
 import propSchema from '../common/PropTypes';
 import ManageBasic from './ManageBasic';
 import ManageTrucks from './ManageTrucks';
+import { actions as userActions } from '../../redux/user';
 
-const ManageBrand = props => (
-  <Tabs>
-    <Tab label="Change Basic Brand Info">
-      <ManageBasic
-        brandId={props.brandId}
-        getBrand={props.getBrand}
-      />
-    </Tab>
-    <Tab label="Manage Your Trucks">
-      <ManageTrucks
-        brandId={props.brandId}
-        trucks={props.trucks}
-        getBrand={props.getBrand}
-      />
-    </Tab>
-  </Tabs>
-);
+class ManageBrand extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: '',
+      food_genre_id: 0,
+    };
+
+    this.handleInfoEdit = this.handleInfoEdit.bind(this);
+    this.handleReduxUpdate = this.handleReduxUpdate.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  handleInfoEdit(data) {
+    const update = {};
+    if (data.name !== '') {
+      update.name = data.name;
+    }
+    if (data.description !== '') {
+      update.description = data.description;
+    }
+    if (data.food_genre_id > 0) {
+      update.food_genre_id = data.food_genre_id;
+    }
+    if (Object.keys(update).length > 0) {
+      axios.put(`/api/brands/${this.props.brandId}`, update)
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }
+
+    console.log('newdata', update);
+    axios.put(`/api/brands/${this.props.brandId}`, update)
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  handleReduxUpdate(data) {
+    const newBrandInfo = Object.assign({}, this.props.user.brands[0]);
+    if (data.name !== '') {
+      newBrandInfo.name = data.name;
+    }
+    if (data.description !== '') {
+      newBrandInfo.description = data.description;
+    }
+    if (data.food_genre_id > 0) {
+      newBrandInfo.food_genre_id = data.food_genre_id;
+    }
+    this.props.userActions.brandInfoUpdate(newBrandInfo);
+  }
+
+  handleSave(data) {
+    this.handleInfoEdit(data);
+    this.handleReduxUpdate(data);
+    this.props.getBrand(this.props.brandId);
+  }
+
+  render() {
+    return (
+      <Tabs>
+        <Tab label="Change Basic Brand Info">
+          <ManageBasic
+            brandId={this.props.brandId}
+            getBrand={this.props.getBrand}
+            onSubmit={this.handleSave}
+            foodGenres={this.props.foodGenres}
+          />
+        </Tab>
+        <Tab label="Manage Your Trucks">
+          <ManageTrucks
+            brandId={this.props.brandId}
+            trucks={this.props.trucks}
+            getBrand={this.props.getBrand}
+          />
+        </Tab>
+      </Tabs>
+    );
+  }
+}
 
 ManageBrand.propTypes = {
   trucks: propSchema.trucks,
   brandId: propSchema.brandId,
   getBrand: propSchema.getBrand,
+  foodGenres: propSchema.foodGenres,
+  userActions: propSchema.userActions,
+  user: propSchema.user,
 };
 
-export default ManageBrand;
+const mapStateToProps = state => ({
+  user: state.user,
+  foodGenres: state.foodGenresReducer.foodGenres,
+});
+
+const mapDispatchToProps = dispatch => ({
+  userActions: bindActionCreators(userActions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageBrand);
