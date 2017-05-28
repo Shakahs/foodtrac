110d616be79236016f79c578dca975cf2ca41444
Keebin <kirikliu@gmail.com>
2017-05-27 20:27:58 -0700

    Refactor add brand component in settings to add brand to current user

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import propSchema from '../common/PropTypes';
+import { actions as userActions } from '../../redux/user';
 import SettingsSideBar from './SettingsSideBar';
 import AddBrandsView from '../common/AddBrandsView';
 
-const Settings = () => (
+const Settings = props => (
   <div>
     <p>Settings</p>
     <SettingsSideBar />
     <Switch>
-      <Route path="/settings/addbrand" component={AddBrandsView} />
+      <Route
+        path="/settings/addbrand"
+        render={() =>
+          (<AddBrandsView
+            foodGenres={props.foodGenres}
+            user={props.user}
+            userActions={props.userActions}
+          />)}
+      />
     </Switch>
   </div>
 );
 
-export default Settings;
+const mapStateToProps = ({ foodGenresReducer, user }) => {
+  const { foodGenres } = foodGenresReducer;
+  return { foodGenres, user };
+};
+
+const mapDispatchToProps = dispatch => ({
+  userActions: bindActionCreators(userActions, dispatch),
+});
+
+Settings.propTypes = {
+  userActions: propSchema.userActions,
+  foodGenres: propSchema.foodGenres,
+  user: propSchema.user,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
