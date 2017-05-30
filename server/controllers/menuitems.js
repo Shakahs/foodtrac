bb8d148dac6d236a76fec21242bc84c20dcb109e
Keebin <kirikliu@gmail.com>
2017-05-30 14:26:50 -0700

    Add menuitems to api spec and write controllers

--- server/controllers/menuitems.js
+++ server/controllers/menuitems.js
@@ -1,15 +1,10 @@
-const MenuItems = require('../../../db/menuitems.model');
+const MenuItems = require('../db/menuitems.model');
 
 module.exports = {
   post(req, res) {
-    req.body.brand_id = req.params.brandId;
     MenuItems.query()
       .insert(req.body)
       .then(item => res.status(201).send(item))
       .catch(e => res.status(400).send(e.message));
-  },
-  put(req, res) {
-  },
-  delete(req, res) {
   },
 };
---
+++ server/controllers/menuitems/{menuItemId}.js
@@ -0,0 +1,18 @@
+const MenuItems = require('../../db/menuitems.model');
+
+module.exports = {
+  put(req, res) {
+    MenuItems.query()
+      .findById(req.params.menuItemId)
+      .patch(req.body)
+      .then(() => MenuItems.query().findById(req.params.menuItemId))
+      .then(item => res.status(200).send(item))
+      .catch(e => res.status(400).send(e.message));
+  },
+  delete(req, res) {
+    MenuItems.query()
+      .deleteById(req.params.menuItemId)
+      .then(() => res.sendStatus(200))
+      .catch(e => res.status(400).send(e.message));
+  },
+};
