60ded23aebed8e75ed34c24399a2337acc6328af
Keebin <kirikliu@gmail.com>
2017-05-30 21:10:12 -0700

    Create reusable comment text input component

---
+++ src/components/Profile/CommentInput.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { TextField } from 'redux-form-material-ui';
+import RaisedButton from 'material-ui/RaisedButton';
+import propSchema from '../common/PropTypes';
+
+const CommentInput = props => (
+  <form onSubmit={props.handleSubmit}>
+    <Field
+      name={props.name}
+      component={TextField}
+      type="text"
+      hintText="Leave a comment!"
+      floatingLabelText="Leave a comment!"
+      fullWidth
+      multiLine
+      rows={3}
+      rowsMax={4}
+      required
+    />
+    <RaisedButton type="submit" primary label="Submit" />
+  </form>
+);
+
+CommentInput.propTypes = {
+  name: propSchema.name,
+  handleSubmit: propSchema.handleSubmit,
+};
+
+export default reduxForm({ form: 'comment' })(CommentInput);
