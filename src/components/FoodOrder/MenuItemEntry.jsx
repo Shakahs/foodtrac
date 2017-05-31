0b10dd4561723c0be84b58cab6232d5be4d4e255
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-05-31 16:34:22 -0700

    Add temp map on order item entry

--- src/components/FoodOrder/MenuItemEntry.jsx
+++ src/components/FoodOrder/MenuItemEntry.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const MenuItemEntry = () => (
-  <div />
+  <div>Some menu item</div>
 );
 
 export default MenuItemEntry;
--- src/components/FoodOrder/MenuItemsList.jsx
+++ src/components/FoodOrder/MenuItemsList.jsx
@@ -3,11 +3,15 @@ import { Paper } from 'material-ui';
 import { Col } from 'react-flexbox-grid';
 import MenuItemEntry from './MenuItemEntry';
 
+const temp = [1, 2, 3, 4, 5, 6];
+
 const MenuItemsList = () => (
   <Col xs={8} sm={8} md={8} lg={8}>
     <Paper>
       <div>Pick your Order</div>
-      <MenuItemEntry />
+      {temp.map(menuItem =>
+        <MenuItemEntry menuItem={menuItem} />,
+      )}
     </Paper>
   </Col>
 );
