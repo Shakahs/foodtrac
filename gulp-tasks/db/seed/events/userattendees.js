729135d2117660f7a9a869691ee7d1125b8f54bc
Shakeel Saleemi <shakahs@gmail.com>
2017-05-31 19:39:13 -0700

    Setup event user attendee seeding

---
+++ gulp-tasks/db/seed/events/userattendees.js
@@ -0,0 +1,44 @@
+const jsf = require('json-schema-faker');
+const Faker = require('faker');
+const Chance = require('chance');
+const Users = require('../../../../server/db/users.model');
+const Events = require('../../../../server/db/events/events.model');
+const { insertSeed, checkSeededTable, provideModelWithKnex } = require('../../../../dbutil');
+
+const chance = new Chance();
+jsf.extend('faker', () => Faker);
+jsf.extend('chance', () => chance);
+
+module.exports = {
+  fn() {
+    let userList = [];
+    let eventList = [];
+    const boundUsers = provideModelWithKnex(Users);
+    const boundEvents = provideModelWithKnex(Events);
+    return boundUsers.query()
+      .then((res) => {
+        userList = res;
+        return boundUsers.knex().destroy();
+      })
+      .then(() => boundEvents.query())
+      .then((res) => {
+        eventList = res;
+        return boundEvents.knex().destroy();
+      })
+      .then(() => {
+        const seedAttendees = [];
+        userList.forEach((user) => {
+          if (chance.bool()) {
+            const attendee = {
+              event_id: chance.pickone(eventList).id,
+              user_id: user.id,
+            };
+            seedAttendees.push(attendee);
+          }
+        });
+        return seedAttendees;
+      })
+      .then(seedData => insertSeed('UserAttendees', seedData))
+      .then(() => checkSeededTable(Events));
+  },
+};
--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ jsf.extend('faker', () => Faker);
 gulp.task('db', (cb) => {
   runSequence('db:recreate', ['db:seed:users', 'db:seed:foodgenres', 'db:seed:locations'], 'db:seed:brands',
     'db:seed:trucks', ['db:seed:locationtimelines', 'db:seed:menuitems', 'db:seed:brandcomments', 'db:seed:events'],
-    'db:seed:upvotes', cb);
+    ['db:seed:upvotes', 'db:seed:events:userattendees'], cb);
 });
 
 /*
