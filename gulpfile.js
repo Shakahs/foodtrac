3a89f3779b32d6c0aa43956bfeb7eab28449fa6f
Shakeel Saleemi <shakahs@gmail.com>
2017-05-31 18:24:25 -0700

    Add event seeding

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,8 @@ jsf.extend('faker', () => Faker);
 
 gulp.task('db', (cb) => {
   runSequence('db:recreate', ['db:seed:users', 'db:seed:foodgenres', 'db:seed:locations'], 'db:seed:brands',
-    'db:seed:trucks', 'db:seed:locationtimelines', 'db:seed:menuitems', 'db:seed:brandcomments', 'db:seed:upvotes', cb);
+    'db:seed:trucks', ['db:seed:locationtimelines', 'db:seed:menuitems', 'db:seed:brandcomments', 'db:seed:events'],
+    'db:seed:upvotes', cb);
 });
 
 /*
