7f1c1de8dbef02815b0a9f396e22fad713df66d5
Shakeel Saleemi <shakahs@gmail.com>
2017-05-31 17:52:15 -0700

    Move locationtimelines seeding out of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,8 @@ const jsf = require('json-schema-faker');
 const Promise = require('bluebird');
 const Chance = require('chance');
 const Faker = require('faker');
-const moment = require('moment');
 const Users = require('./server/db/users.model');
 const Brands = require('./server/db/brands.model');
-const Trucks = require('./server/db/trucks.model');
-const Locations = require('./server/db/locations.model');
-const LocationTimelines = require('./server/db/locationtimelines.model');
 const MenuItems = require('./server/db/menuitems.model');
 const BrandComments = require('./server/db/brandcomments.model');
 const gulpRequireTasks = require('gulp-require-tasks');
@@ -45,45 +41,6 @@ jsf.extend('faker', () => Faker);
 gulp.task('db', (cb) => {
   runSequence('db:recreate', ['db:seed:users', 'db:seed:foodgenres', 'db:seed:locations'], 'db:seed:brands',
     'db:seed:trucks', 'db:seed:locationtimelines', 'db:seed:menuitems', 'db:seed:brandcomments', cb);
-});
-
-gulp.task('db:seed:locationtimelines', () => {
-  const boundTruck = provideModelWithKnex(Trucks);
-  const boundLocations = provideModelWithKnex(Locations);
-  let truckList = [];
-  let locationList = [];
-  return boundTruck.query()
-    .then((res) => {
-      truckList = res;
-      return boundTruck.knex().destroy();
-    })
-    .then(() => boundLocations.query())
-    .then((res) => {
-      locationList = res;
-      return boundLocations.knex().destroy();
-    })
-    .then(() => {
-      const accumulatedSeedData = [];
-      truckList.forEach((truck) => {
-        const lastSetStart = moment.utc();
-        for (let i = 0; i < 5; i++) {
-          const newSeedDataItem = {};
-          lastSetStart.subtract(chance.integer({ min: 4, max: 7 }), 'hours')
-            .subtract(chance.integer({ min: 0, max: 59 }), 'minutes');
-          newSeedDataItem.start = moment(lastSetStart);
-          newSeedDataItem.end = (chance.bool()) ? moment(newSeedDataItem.start).add(chance.integer({ min: 1, max: 3 }), 'hours')
-            .add(chance.integer({ min: 0, max: 59 }), 'minutes').format('YYYY-MM-DD HH:mm:ss') : 0;
-          newSeedDataItem.start = newSeedDataItem.start.format('YYYY-MM-DD HH:mm:ss');
-          newSeedDataItem.location_id = chance.pickone(locationList).id;
-          newSeedDataItem.truck_id = truck.id;
-          newSeedDataItem.checked_in = true;
-          accumulatedSeedData.push(newSeedDataItem);
-        }
-      });
-      return accumulatedSeedData;
-    })
-    .then(seedData => insertSeed('LocationTimelines', seedData))
-    .then(() => checkSeededTable(LocationTimelines));
 });
 
 gulp.task('db:seed:menuitems', () => {
