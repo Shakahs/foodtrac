c0483c5502685793c9dec3c4870323781ad05d77
Shakeel Saleemi <shakahs@gmail.com>
2017-06-02 10:19:40 -0700

    Setup event detail api controller

---
+++ server/controllers/events/{eventId}.js
@@ -0,0 +1,11 @@
+const Events = require('../../db/events/events.model');
+
+module.exports = {
+  get(req, res) {
+    Events.query()
+      .findById(req.params.eventId)
+      .eager('[locations, users_attending, brands_attending, comments]')
+      .then(event => res.status(200).send(event))
+      .catch(e => res.status(400).send(e.message));
+  },
+};
