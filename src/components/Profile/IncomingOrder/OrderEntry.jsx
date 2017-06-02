cfe248303d928ec4444e654fc7e9b90e4d82acd3
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-06-01 21:57:42 -0700

    Add total price to incoming orders

--- src/components/Profile/IncomingOrder/OrderEntry.jsx
+++ src/components/Profile/IncomingOrder/OrderEntry.jsx
@@ -8,12 +8,21 @@ class OrderEntry extends Component {
     super();
     this.state = {};
     this.orderReady = this.orderReady.bind(this);
+    this.calculateTotal = this.calculateTotal.bind(this);
   }
 
   orderReady() {
     axios.put(`/api/orders/${this.props.order.id}`, { ready: true })
       .then(() => this.props.getOrders(this.props.truckId))
       .catch(e => console.log(e));
+  }
+
+  calculateTotal() {
+    let total = 0;
+    this.props.order.orderitems.forEach((item) => {
+      total += item.menu_item.price;
+    });
+    return total / 100;
   }
 
   render() {
@@ -23,6 +32,7 @@ class OrderEntry extends Component {
         {this.props.order.orderitems.map(({ menu_item }) =>
           <div>{menu_item.name}</div>,
         )}
+        Total due: ${this.calculateTotal()}
         <FlatButton
           label="Order ready"
           onClick={this.orderReady}
