9b2524dc03249de1e17c85d308b890d5646bbe2a
Shakeel Saleemi <shakahs@gmail.com>
2017-06-02 17:55:21 -0700

    Add logic to controller to handle duplicate attendance requests

--- server/controllers/events/{eventId}/user_attendance.js
+++ server/controllers/events/{eventId}/user_attendance.js
@@ -8,6 +8,7 @@ module.exports = {
         user_id: req.body.userId,
       })
       .then(newFollow => res.status(201).send(newFollow))
+      .catch({ code: 'ER_DUP_ENTRY' }, () => res.status(409).send('user already attending'))
       .catch(e => res.status(400).send(e.message));
   },
   delete(req, res) {
@@ -15,7 +16,6 @@ module.exports = {
       .delete()
       .where('event_id', '=', req.params.eventId)
       .where('user_id', '=', req.body.userId)
-
       .then(() => res.status(202).send('No longer attending'))
       .catch(e => res.status(400).send(e.message));
   },
