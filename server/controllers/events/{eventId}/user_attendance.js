32c80f59f761d5275f1177e557b292be581eb8c0
Shakeel Saleemi <shakahs@gmail.com>
2017-06-02 17:33:19 -0700

    Write controller for user to register attendance to event

---
+++ server/controllers/events/{eventId}/user_attendance.js
@@ -0,0 +1,22 @@
+const UserAttendees = require('../../../db/events/userattendees.model');
+
+module.exports = {
+  post(req, res) {
+    UserAttendees.query()
+      .insert({
+        event_id: req.params.eventId,
+        user_id: req.body.userId,
+      })
+      .then(newFollow => res.status(201).send(newFollow))
+      .catch(e => res.status(400).send(e.message));
+  },
+  delete(req, res) {
+    UserAttendees.query()
+      .delete()
+      .where('event_id', '=', req.params.eventId)
+      .where('user_id', '=', req.body.userId)
+
+      .then(() => res.status(202).send('No longer attending'))
+      .catch(e => res.status(400).send(e.message));
+  },
+};
