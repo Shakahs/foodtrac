fadff9db20e9e6099b39942cda59106102d85168
Shakeel Saleemi <shakahs@gmail.com>
2017-06-03 21:39:02 -0700

    Truck owner bylines displaying on User Emblems

--- src/components/common/Emblem/UserEmblem.jsx
+++ src/components/common/Emblem/UserEmblem.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Avatar from 'material-ui/Avatar';
 import 'font-awesome/css/font-awesome.min.css';
 import FontIcon from 'material-ui/FontIcon';
+import { Link } from 'react-router-dom';
 import Emblem from './Emblem';
 import propSchema from '../../common/PropTypes';
+import './Emblem.scss';
 
 class UserEmblem extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const { user } = this.props;
+
     const userAvatar = (<Avatar
       icon={<FontIcon
         className="fa fa-user"
@@ -14,7 +18,14 @@ class UserEmblem extends React.Component { // eslint-disable-line react/prefer-s
     />);
     return (
       <Emblem avatar={userAvatar}>
-        {this.props.user.first_name} {this.props.user.last_name}
+        {user.first_name} {user.last_name}
+        {user.is_truck_owner &&
+        <div className="truckOwnerByline">
+          <Link to={`/brand/${user.brands[0].id}`} >
+            <FontIcon className="fa fa-truck" style={{ fontSize: '1.2em' }} /> {user.brands[0].name}
+          </Link>
+          </div>
+        }
       </Emblem>)
     ;
   }
