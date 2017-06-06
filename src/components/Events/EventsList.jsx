752356ded82c971037bbb0fcc788e75cebbe6e44
Shakeel Saleemi <shakahs@gmail.com>
2017-06-06 03:21:56 -0700

    Refactor EventsList component to do API call in componentDidMount instead of using react-refetch

--- src/components/Events/EventsList.jsx
+++ src/components/Events/EventsList.jsx
@@ -1,39 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-refetch';
 import { Grid } from 'react-flexbox-grid';
 import _ from 'lodash';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import axios from 'axios';
 import EventsListEntry from './EventsListEntry';
+import { actions as userActions } from '../../redux/user';
 
 class EventsList extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    const { eventsFetch } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    };
+  }
 
-    if (eventsFetch.fulfilled) {
-      return (
-        <Grid fluid>
-          <div>
-            <Link to={'/events/create'}>Create an event</Link><br />
-          </div>
-          {this.props.eventsFetch.value.length > 0 ?
-            _.map(this.props.eventsFetch.value, event => <EventsListEntry event={event} />)
+  componentDidMount() {
+    axios.get('/api/events/')
+      .then(({ data }) => {
+        this.setState({ events: data });
+      });
+  }
+
+  render() {
+    return (
+      <Grid fluid>
+        <div>
+          <Link to={'/events/create'}>Create an event</Link><br />
+        </div>
+        {this.state.events.length > 0 ?
+            _.map(this.state.events, event => <EventsListEntry event={event} />)
             :
             <div className="noItems">This truck is not attending any events</div>
           }
-        </Grid>
-      );
-    }
-    return (<div>Loading</div>);
+      </Grid>
+    );
   }
 }
 
 EventsList.propTypes = {
-  eventsFetch: PropTypes.shape({
-    value: PropTypes.array,
-  }),
+
 };
 
-export default connect(() => ({
-  eventsFetch: '/api/events/',
-}))(EventsList);
+const mapStateToProps = state => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = dispatch => ({
+  userActions: bindActionCreators(userActions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsList);
