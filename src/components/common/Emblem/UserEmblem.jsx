3acf22b9ed1d1cc745bd46f71ba0550cfaa5955f
Keebin <kirikliu@gmail.com>
2017-06-07 20:38:01 -0700

    Add check in UserEmblem for truck owners with no brands

--- src/components/common/Emblem/UserEmblem.jsx
+++ src/components/common/Emblem/UserEmblem.jsx
@@ -17,7 +17,7 @@ class UserEmblem extends React.Component { // eslint-disable-line react/prefer-s
         <div>
           <FontIcon className="fa fa-star" style={{ fontSize: '1.2em' }} /> {user.brand_reviews ? user.brand_reviews.length : 0} Reviews
       </div>
-        {user.is_truck_owner > 0 && user.brands && (<div className="truckOwnerByline">
+        {user.is_truck_owner > 0 && user.brands && user.brands.length > 0 && (<div className="truckOwnerByline">
           <Link to={`/brand/${user.brands[0].id}`} >
             <FontIcon className="fa fa-truck" style={{ fontSize: '1.2em' }} /> {user.brands[0].name}
           </Link>
