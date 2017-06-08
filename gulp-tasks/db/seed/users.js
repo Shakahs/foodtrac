bd82b991c2abe70d04f83838ec4332cfa3339745
Shakeel Saleemi <shakahs@gmail.com>
2017-06-08 16:12:48 -0700

    Tweak number of seed users/trucks

--- gulp-tasks/db/seed/users.js
+++ gulp-tasks/db/seed/users.js
@@ -22,8 +22,8 @@ module.exports = {
     const auth0Results = {};
     const userSeedSchema = {
       type: 'array',
-      minItems: 40,
-      maxItems: 80,
+      minItems: 200,
+      maxItems: 300,
       uniqueItems: true,
       items: Users.jsonSchema,
     };
