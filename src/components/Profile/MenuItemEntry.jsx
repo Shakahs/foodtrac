7048800e4b645a1f1aa2cbff5e592dd47b2dee8d
Hugo Dolemieux <hugodolemieux@verizon.net>
2017-06-09 14:00:26 -0700

    Styles menu page

--- src/components/Profile/MenuItemEntry.jsx
+++ src/components/Profile/MenuItemEntry.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
+import { Paper } from 'material-ui';
 import propSchema from '../common/PropTypes';
+import './menu.scss';
 
 const MenuItemEntry = props => (
-  <Row>
-    <Col xs={3} sm={3} md={3} lg={3}>
-      {props.item.name}
-    </Col>
-    <Col xs={4} sm={4} md={4} lg={4}>
-      {props.item.description}
-    </Col>
-    <Col xs={2} sm={2} md={2} lg={2}>
+  <Paper className="menuItem">
+    <Row>
+      <Col xs={5} sm={5} md={5} lg={5}>
+        {props.item.name}
+      </Col>
+      <Col xs={5} sm={5} md={5} lg={5}>
+        {props.item.description}
+      </Col>
+      <Col xs={2} sm={2} md={2} lg={2}>
       ${props.item.price}
-    </Col>
-    {props.item.calories !== 0 ?
-      <Col xs={3} sm={3} md={3} lg={3}>
-        Calories: {props.item.calories}
-      </Col> : null
-    }
-  </Row>
+      </Col>
+    </Row>
+  </Paper>
 );
 
 MenuItemEntry.propTypes = {
