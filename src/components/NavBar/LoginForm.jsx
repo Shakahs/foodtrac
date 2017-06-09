47bd6f2acab4dd948920f188a7ca0a51bc88e0e8
Shakeel Saleemi <shakahs@gmail.com>
2017-06-08 22:46:49 -0700

    Style the LoginForm a little bit

--- src/components/NavBar/LoginForm.jsx
+++ src/components/NavBar/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import RaisedButton from 'material-ui/RaisedButton';
 import { Field, reduxForm } from 'redux-form';
 import propSchema from '../common/PropTypes';
 
@@ -10,20 +12,36 @@ import propSchema from '../common/PropTypes';
 // );
 
 const LoginFormComponent = props => (
-  <form onSubmit={props.handleSubmit}>
-    <div>
-      <label htmlFor="email">Email</label>
-      <Field name="email" component="input" type="text" />
-    </div>
-    <div>
-      <label htmlFor="password">Password</label>
-      <Field name="password" component="input" type="text" />
-    </div>
-    <button type="submit" onClick={() => { props.handleSubmit(); }}>Login</button>
-    <Link to="/signup">
-      <p>Sign Up</p>
-    </Link>
-  </form>
+  <Grid fluid className="loginPopover">
+    <form onSubmit={props.handleSubmit}>
+      <Row>
+        <Col >
+          <label htmlFor="email">Email:</label>
+        </Col>
+        <Col xs={5} className="loginRight">
+          <Field name="email" component="input" type="text" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <label htmlFor="password">Password:</label>
+        </Col>
+        <Col xs={5} className="loginRight">
+          <Field name="password" component="input" type="text" />
+        </Col>
+      </Row>
+      <Row around="xs">
+        <Col>
+          <RaisedButton label="Login" primary onClick={() => { props.handleSubmit(); }} />
+        </Col>
+        <Col>
+          <Link to="/signup">
+            <RaisedButton label="Sign up" secondary />
+          </Link>
+        </Col>
+      </Row>
+    </form>
+  </Grid>
 );
 
 LoginFormComponent.propTypes = {
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 import DashBoard from './DashBoard/DashBoard';
 import MapPage from './MapPage/MapPage';
 import Profile from './Profile/Profile';
-import Login from './Login/Login';
 import Settings from './Settings/Settings';
 import FoodOrder from './FoodOrder/FoodOrder';
 import SignUp from './SignUp';
@@ -22,7 +21,6 @@ class Routes extends Component {
           <Route exact path="/" component={DashBoard} />
           <Route path="/map" component={MapPage} />
           <Route path="/brand/:brandId" component={Profile} />
-          <Route path="/login" component={Login} />
           <Route path="/settings" component={Settings} />
           <Route path="/signup" component={SignUp} />
           <Route path="/order/:truckId" component={FoodOrder} />
