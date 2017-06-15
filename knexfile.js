2b844335d390c45f2b112e2fed2b6c1b0d753465
Keebin <kirikliu@gmail.com>
2017-06-15 09:51:04 -0700

    Edit knexfile to use local mysql server

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,8 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: process.env.DATABASE_HOST,
-      user: 'foodtrac',
-      password: 'foodtracdata',
+      user: 'root',
+      password: '',
       database: 'foodtrac',
       multipleStatements: true,
       timezone: 'utc',
